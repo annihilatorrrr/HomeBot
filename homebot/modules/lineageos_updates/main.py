a58f6bba640db9fad82f364ada2e14dc25dfa350
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2021-08-27 02:38:36 +0200

    homebot: modules: lineageos_updater: Get info about observer
    Change-Id: I0577f07f5ccff99d006cbc6ac1f8bdab2e28fb59

--- homebot/modules/lineageos_updates/main.py
+++ homebot/modules/lineageos_updates/main.py
@@ -1,3 +1,4 @@
+from datetime import datetime
 from homebot.lib.libadmin import user_is_admin
 from homebot.modules.lineageos_updates.observer import observer
 from homebot.modules.lineageos_updates.poster import Poster, posters
@@ -23,13 +24,27 @@ def lineageos_updater(update: Update, context: CallbackContext):
 
 	command = context.args[0]
 
-	if command == "enable":
+	if command == "info":
+		alive = observer.thread.is_alive()
+		reply = f"Enabled: {str(alive)}\n"
+		if alive:
+			reply += (
+				f"Observed devices:\n"
+				f"\n"
+				f"Device | Last update\n"
+			)
+			for device in observer.last_device_update:
+				date = datetime.fromtimestamp(observer.last_device_update[device])
+				reply += f"{device} | {date.year}/{date.month}/{date.day}\n"
+	elif command == "enable":
 		observer.event.set()
-		update.message.reply_text("Observer enabled")
+		reply = "Observer enabled"
 	elif command == "disable":
 		observer.event.clear()
-		update.message.reply_text("Observer disabled")
+		reply = "Observer disabled"
 	else:
-		update.message.reply_text("Error: Unknown command")
+		reply = "Error: Unknown command"
+
+	update.message.reply_text(reply)
 
 	return
