414a3c3cfc8e55d1345346a4b96f4ac05b11f648
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2021-08-28 15:04:57 +0200

    homebot: core: database: Initial release
    * Since we assume cwd is writable, we can use it to create a database
    
    Change-Id: I5ad8e7c7350e19baea73f57b57213cf40ed3ac90

---
+++ homebot/core/database.py
@@ -0,0 +1,31 @@
+import pickle
+from threading import Lock
+
+DATABASE_FILE_NAME = "data.pkl"
+
+class HomeBotDatabase:
+	def __init__(self):
+		self.fd = open(DATABASE_FILE_NAME, 'wb')
+
+		self.data = pickle.load(self.fd)
+		self.lock = Lock()
+
+	def get(self, key, default=None):
+		with self.lock:
+			if not key in self.data:
+				return default
+
+			return self.data[key]
+
+	def set(self, key, value):
+		with self.lock:
+			self.data[key] = value
+			self._sync()
+
+	def _sync(self):
+		pickle.dump(self.data, self.fd)
+
+# Only one database is allowed for now
+# TODO: Set custom names for bot instances and
+# allow to have one database per bot
+database = HomeBotDatabase()
