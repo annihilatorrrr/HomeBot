3eff2b1d240573d30f6c75f4a0748918454ccc78
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2021-08-31 02:03:18 +0200

    homebot: modules: lineageos_updater: Add when command
    Change-Id: Id3270d26aee081e3b534b97c07d2a3b32e21601c

--- homebot/modules/lineageos_updates/main.py
+++ homebot/modules/lineageos_updates/main.py
@@ -1,7 +1,10 @@
+from calendar import day_name
 from datetime import datetime
 from homebot.lib.libadmin import user_is_admin
 from homebot.modules.lineageos_updates.observer import observer
 from homebot.modules.lineageos_updates.poster import Poster, posters
+from shutil import which
+from subprocess import check_output
 from telegram.bot import Bot
 from telegram.ext import CallbackContext
 from telegram.update import Update
@@ -24,7 +27,18 @@ def lineageos_updater(update: Update, context: CallbackContext):
 
 	command = context.args[0]
 
-	if command == "info":
+	if command == "when":
+		if len(context.args) < 2:
+			reply = f"Device codename not specified"
+		elif which("python2") is None:
+			reply = f"Python 2.x isn't installed, it is required to get the random int"
+		else:
+			device = context.args[1]
+			command = f'from random import Random; print(Random("{device}").randint(1, 7))'
+			day_int = int(check_output(f"python2 -c '{command}'", shell=True))
+			day = day_name[day_int - 1]
+			reply = f"The next build for {device} will be on {day}"
+	elif command == "info":
 		alive = observer.thread.is_alive()
 		reply = f"Enabled: {str(alive)}\n"
 		if alive:
