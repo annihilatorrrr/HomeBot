635c9c829b6329f492b11998d8b91beec7a4c46d
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2021-09-07 03:29:28 +0200

    homebot: core: database: Move to JSON
    * Some improvements as well
    
    Change-Id: I05fd4afe5bf5d273b629bd9dcada665d0497374f

--- homebot/core/database.py
+++ homebot/core/database.py
@@ -1,41 +1,73 @@
-import pickle
+import json
+from pathlib import Path
 from threading import Lock
 
-DATABASE_FILE_NAME = "data.pkl"
+DATABASE_FILE_NAME = "data.json"
 
 ALLOWED_DATA_TYPES = [
 	bool,
 	dict,
 	float,
 	int,
+	list,
+	# TODO: With 3.10 move to types.NoneType
+	type(None),
 	str,
 ]
 
-class HomeBotDatabase:
+class HomeBotDatabase(dict):
 	def __init__(self):
 		"""Initialize the database."""
-		self.fd = open(DATABASE_FILE_NAME, 'wb')
+		super().__init__()
+		self.file_path = Path(DATABASE_FILE_NAME)
+		self.data_lock = Lock()
+		self.file_lock = Lock()
 
-		self.data = pickle.load(self.fd)
-		self.lock = Lock()
+		if self.file_path.is_file():
+			self._load()
 
-	def get(self, key, default=None):
-		with self.lock:
-			if not key in self.data:
-				return default
+		self._dump()
 
-			return self.data[key]
+	def __getitem__(self, k: str):
+		if type(k) is not str:
+			raise TypeError("Key isn't a string")
 
-	def set(self, key, value):
-		if type(value) not in ALLOWED_DATA_TYPES:
+		with self.data_lock:
+			if not '.' in k:
+				return super().__getitem__(k)
+			else:
+				value = dict(self)
+				for subkey in k.split('.'):
+					value = value[subkey]
+
+			return value
+
+	def __setitem__(self, k: str, v):
+		if type(k) is not str:
+			raise TypeError("Key isn't a string")
+
+		if type(v) not in ALLOWED_DATA_TYPES:
 			raise TypeError("Value data type not allowed")
 
-		with self.lock:
-			self.data[key] = value
-			self._sync()
+		with self.data_lock:
+			if not '.' in k:
+				return super().__setitem__(k, v)
+			else:
+				d = v
+				for subkey in k.split('.')[::-1]:
+					d = {subkey: d}
 
-	def _sync(self):
-		pickle.dump(self.data, self.fd)
+				self.update(d)
+
+			self._dump()
+
+	def _load(self):
+		with self.file_lock:
+			self.update(json.loads(self.file_path.read_bytes()))
+
+	def _dump(self):
+		with self.file_lock:
+			self.file_path.write_text(json.dumps(self, indent=4, sort_keys=True))
 
 # Only one database is allowed for now
 # TODO: Set custom names for bot instances and
