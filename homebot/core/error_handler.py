b8420fe3df66c478fe990d09a1b2d46bba4b0217
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2021-09-07 03:32:19 +0200

    homebot: core: error_handler: Improve syntax
    Change-Id: Ic0bd3e185f0f7e9f0a42931fa187368775bc6c70

--- homebot/core/error_handler.py
+++ homebot/core/error_handler.py
@@ -9,9 +9,9 @@ def format_exception(exception):
 												   limit=None, chain=True))
 
 def error_handler(update: Update, context: CallbackContext):
-	formatted_error =   "HomeBot: Error encountered!\n"
-	formatted_error += f"Command sent: {update.message.text}\n\n"
-	formatted_error +=  format_exception(context.error)
+	formatted_error = ("HomeBot: Error encountered!\n"
+	                   f"Command sent: {update.message.text}\n\n"
+	                   f"{format_exception(context.error)}")
 	LOGE(formatted_error)
 	update.message.reply_text(formatted_error)
 	LOGE("End error handling")
