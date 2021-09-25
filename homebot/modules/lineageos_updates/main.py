c5bb0cc2c8453a3feffd7e7af3c3fcab005a1460
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2021-09-25 19:12:50 +0200

    homebot: modules: lineageos_updates: Less cringe sub-functions handler
    Change-Id: I2543c3e51df35104668a72d7a15ff0fc13a3f79b

--- homebot/modules/lineageos_updates/main.py
+++ homebot/modules/lineageos_updates/main.py
@@ -9,17 +9,7 @@ from subprocess import check_output
 from telegram.bot import Bot
 from telegram.ext import CallbackContext
 from telegram.update import Update
-
-ADMIN_COMMANDS = [
-	"enable",
-	"disable",
-]
-
-ALL_COMMANDS = [
-	"last",
-	"when",
-	"info",
-] + ADMIN_COMMANDS
+from typing import Callable
 
 def add_user(bot: Bot):
 	posters[bot] = Poster(bot)
@@ -28,70 +18,95 @@ def remove_user(bot: Bot):
 	if bot in posters:
 		del posters[bot]
 
+def disable(update: Update, context: CallbackContext):
+	observer.event.clear()
+	update.message.reply_text("Observer disabled")
+
+def enable(update: Update, context: CallbackContext):
+	observer.event.set()
+	update.message.reply_text("Observer enabled")
+
+def info(update: Update, context: CallbackContext):
+	alive = observer.thread.is_alive()
+	text = f"Enabled: {str(alive)}\n"
+	if alive:
+		text += (
+			"Observed devices:\n"
+			"Device | Last post\n"
+		)
+		for device in observer.last_device_post:
+			date = datetime.fromtimestamp(observer.last_device_post[device])
+			text += f"{device} | {date.strftime('%Y/%m/%d, %H:%M:%S')}\n"
+
+	update.message.reply_text(text)
+
+def last(update: Update, context: CallbackContext):
+	if len(context.args) < 2:
+		update.message.reply_text("Device codename not specified")
+		return
+
+	device = context.args[1]
+	response = get_device_updates(device)
+	if not response:
+		update.message.reply_text(f"Error: no updates found for {device}")
+		return
+
+	last_update = response[-1]
+	update.message.reply_text(f"Last update for {device}:\n"
+	                          f"Filename: {last_update['filename']}\n"
+	                          f"Version: {last_update['version']}\n"
+	                          f"Download: {last_update['url']}")
+
+def when(update: Update, context: CallbackContext):
+	if len(context.args) < 2:
+		update.message.reply_text("Device codename not specified")
+		return
+
+	if which("python2") is None:
+		update.message.reply_text("Python 2.x isn't installed, it's required to parse the day")
+		return
+
+	device = context.args[1]
+	command = f'from random import Random; print(Random("{device}").randint(1, 7))'
+	day_int = int(check_output(f"python2 -c '{command}'", shell=True))
+	day = day_name[day_int - 1]
+	update.message.reply_text(f"The next build for {device} will be on {day}")
+
+# name: [function, admin_only]
+COMMANDS: dict[str, list[Callable[[Update, CallbackContext], None], bool]] = {
+	"disable": [disable, True],
+	"enable": [enable, True],
+	"info": [info, False],
+	"last": [last, False],
+	"when": [when, False],
+}
+
+HELP_TEXT = (
+	"Available commands:\n" +
+	"\n".join(COMMANDS.keys())
+)
+
 def lineageos_updates(update: Update, context: CallbackContext):
 	if not context.args:
-		update.message.reply_text("Error: No argument provided")
+		update.message.reply_text(
+			"Error: No argument provided\n\n"
+			f"{HELP_TEXT}"
+		)
 		return
 
 	command = context.args[0]
 
-	if command in ADMIN_COMMANDS:
-		if not user_is_admin(update.message.from_user.id):
-			update.message.reply_text("Error: You are not authorized to interact with LineageOS updater")
-			return
+	if command not in COMMANDS:
+		update.message.reply_text(
+			f"Error: Unknown command {command}\n\n"
+			f"{HELP_TEXT}"
+		)
+		return
 
-	reply = ""
+	func, admin_only = COMMANDS[command]
 
-	# God I would've appreciated a case-switch here
-	if command == "last":
-		if len(context.args) < 2:
-			reply = "Device codename not specified"
+	if admin_only and not user_is_admin(update.message.from_user.id):
+		update.message.reply_text("Error: You are not authorized to use this function")
+		return
 
-		device = context.args[1]
-		response = get_device_updates(device)
-		if not response:
-			reply = f"Error: no updates found for {device}"
-		else:
-			last_update = response[-1]
-			reply = (f"Last update for {device}:\n"
-					f"Filename: {last_update['filename']}\n"
-					f"Version: {last_update['version']}\n"
-					f"Download: {last_update['url']}")
-	elif command == "when":
-		if len(context.args) < 2:
-			reply = "Device codename not specified"
-		elif which("python2") is None:
-			reply = "Python 2.x isn't installed, it is required to get the random int"
-		else:
-			device = context.args[1]
-			command = f'from random import Random; print(Random("{device}").randint(1, 7))'
-			day_int = int(check_output(f"python2 -c '{command}'", shell=True))
-			day = day_name[day_int - 1]
-			reply = f"The next build for {device} will be on {day}"
-	elif command == "info":
-		alive = observer.thread.is_alive()
-		reply = f"Enabled: {str(alive)}\n"
-		if alive:
-			reply += (
-				"Observed devices:\n"
-				"Device | Last post\n"
-			)
-			for device in observer.last_device_post:
-				date = datetime.fromtimestamp(observer.last_device_post[device])
-				reply += f"{device} | {date.strftime('%Y/%m/%d, %H:%M:%S')}\n"
-	elif command == "enable":
-		observer.event.set()
-		reply = "Observer enabled"
-	elif command == "disable":
-		observer.event.clear()
-		reply = "Observer disabled"
-	else:
-		if command != "help":
-			reply = f"Error: Unknown command {command}\n\n"
-
-		reply += "Available commands:\n"
-		reply += "\n".join(ALL_COMMANDS)
-
-	update.message.reply_text(reply)
-
-	return
+	func(update, context)
