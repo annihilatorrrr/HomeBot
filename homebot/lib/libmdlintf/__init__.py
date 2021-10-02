1d83cd0bf9fbe4a1fbaa70d4d76e95c7f285d601
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2021-10-02 11:38:03 +0200

    homebot: lib: mdlintf: Extend register_module
    * Modules shouldn't import core functions if it isn't a core module
    
    Change-Id: I9ab7519170ee7bb689c1b566df7dc70e2c4619a5

---
+++ homebot/lib/libmdlintf/__init__.py
@@ -0,0 +1,6 @@
+"""Module interface library."""
+
+from homebot.core.mdlintf import ModuleInterface, register_module
+
+def register_module(mdlintf: ModuleInterface):
+	return register_module(mdlintf)
