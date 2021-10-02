457e4dacf18bdc17321dbdc601655509ccb9304c
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2021-10-02 11:43:23 +0200

    homebot: lib: libmdlintf: Fixup register_module
    Change-Id: I01989f22e08ef2833e28f06c6678ef295f4aea2e

--- homebot/lib/libmdlintf/__init__.py
+++ homebot/lib/libmdlintf/__init__.py
@@ -1,6 +1,7 @@
 """Module interface library."""
 
-from homebot.core.mdlintf import ModuleInterface, register_module
+from homebot.core.mdlintf import ModuleInterface
+from homebot.core.mdlintf import register_module as _register_module
 
 def register_module(mdlintf: ModuleInterface):
-	return register_module(mdlintf)
+	return _register_module(mdlintf)
