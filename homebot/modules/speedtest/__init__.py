f6d0b5ae2d52b52e057bcec17bdf636b78d6fa99
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2021-11-04 20:33:30 +0100

    homebot: modules: speedtest: Adapt for new changes
    Change-Id: I2dc0a15e3f1d68b0ca46a52b68c52dce9fec3e0d

--- homebot/modules/speedtest/__init__.py
+++ homebot/modules/speedtest/__init__.py
@@ -1,18 +1,17 @@
 """HomeBot speedtest module."""
 
-from homebot.core.mdlintf import (
-	ModuleInterface,
-	register_module,
-)
+from homebot.core.mdlintf import ModuleInterface, mdlbinder
+from telegram.ext import CommandHandler
 
 from homebot.modules.speedtest.main import (
 	speedtest,
 )
 
-@register_module
 class SpeedtestModule(ModuleInterface):
 	name = "speedtest"
 	version = "1.0"
 	commands = {
-		speedtest: ["speedtest"],
+		CommandHandler(["speedtest"], speedtest),
 	}
+
+mdlbinder.register_interface(SpeedtestModule())
