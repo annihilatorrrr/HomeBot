80e37b513212ea83d70e6b52ea42b096e0f751e7
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2021-11-04 20:33:38 +0100

    homebot: modules: xda: Adapt for new changes
    Change-Id: I070c32911b8a0e55142aea54e73f64b7aee5a07c

--- homebot/modules/xda/__init__.py
+++ homebot/modules/xda/__init__.py
@@ -1,18 +1,17 @@
 """HomeBot XDA module."""
 
-from homebot.core.mdlintf import (
-	ModuleInterface,
-	register_module,
-)
+from homebot.core.mdlintf import ModuleInterface, mdlbinder
+from telegram.ext import CommandHandler
 
 from homebot.modules.xda.main import (
 	xda,
 )
 
-@register_module
 class XdaModule(ModuleInterface):
 	name = "xda"
 	version = "1.0"
 	commands = {
-		xda: ["xda"],
+		CommandHandler(["xda"], xda),
 	}
+
+mdlbinder.register_interface(XdaModule())
