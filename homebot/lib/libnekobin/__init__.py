61da4ba70f6fc618b85073c0ffdd777e395ff55e
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2021-12-07 18:15:42 +0100

    homebot: modules: nekobin: Improvements
    Change-Id: I370f2cc6189dd95ba487b9bef0313f7dbda83304

--- homebot/lib/libnekobin/__init__.py
+++ homebot/lib/libnekobin/__init__.py
@@ -4,6 +4,11 @@ URL = "https://nekobin.com"
 
 def to_nekobin(data: str) -> str:
 	"""Upload a string to Nekobin and return its URL."""
-	resp = post(f"{URL}/api/documents", json={"content": data}).json()
-	key = resp.get("result").get("key")
+	data = {"content": data}
+
+	resp = post(f"{URL}/api/documents", json=data)
+	resp.raise_for_status()
+	resp_json = resp.json()
+
+	key = resp_json.get("result").get("key")
 	return f"{URL}/{key}"
--- homebot/modules/nekobin/main.py
+++ homebot/modules/nekobin/main.py
@@ -1,5 +1,6 @@
 from homebot.lib.libnekobin import to_nekobin
 from io import BytesIO
+from requests import HTTPError
 from telegram.ext import CallbackContext
 from telegram.update import Update
 
@@ -9,18 +10,23 @@ def nekobin(update: Update, context: CallbackContext):
 		return
 
 	document = update.message.reply_to_message.document
+	message = update.message.reply_text("Uploading...")
 
 	try:
 		file: BytesIO = document.get_file().download(out=BytesIO())
 	except Exception:
-		update.message.reply_text("Error: failed to download file from Telegram (probably too big)")
+		message.edit_text("Error: failed to download file from Telegram (probably too big)")
 		return
 
+	text = file.getvalue().decode(encoding="utf-8", errors="ignore")
+
 	try:
-		url = to_nekobin(file.getvalue())
-	except Exception:
-		update.message.reply_text("Error: failed to upload file to Nekobin")
+		url = to_nekobin(text)
+	except HTTPError as e:
+		message.edit_text(f"Error: failed to upload file to Nekobin: {e.response.status_code}")
+	except Exception as e:
+		message.edit_text(f"Error: failed to upload file to Nekobin: unknown error")
 	else:
-		update.message.reply_text(f"File uploaded to Nekobin: {url}", disable_web_page_preview=True)
+		message.edit_text(f"File uploaded to Nekobin: {url}", disable_web_page_preview=True)
 	finally:
 		file.close()
