52a38e613d00800ed5e61fe97fcfacafed9ae089
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2021-12-23 11:59:55 +0100

    homebot: lib: libexception: Add docstring
    Change-Id: I5a54a6f247f3c0c2c8d3b712640cb40104d3e6da

--- homebot/lib/libexception/__init__.py
+++ homebot/lib/libexception/__init__.py
@@ -1,6 +1,9 @@
+"""HomeBot exceptions library."""
+
 import traceback
 
 def format_exception(exception):
+	"""Format an exception."""
 	return ''.join(traceback.format_exception(type(exception), exception,
 	                                          exception.__traceback__,
 	                                          limit=None, chain=True))
