ec6e817535a41c7716b82848407b698c064bff71
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2021-12-27 02:27:30 +0100

    homebot: lib: liblineage: Add LINEAGEOS_TO_ANDROID_VERSION
    Change-Id: I6e4c4deb9490c46c56d5b50ef809eebeda5b4325

--- homebot/lib/liblineage/__init__.py
+++ homebot/lib/liblineage/__init__.py
@@ -1,3 +1,15 @@
 """LineageOS utils library."""
 
+from homebot.lib.libandroid import AndroidVersion
+
 GITHUB_ORG = "https://github.com/LineageOS"
+
+LINEAGEOS_TO_ANDROID_VERSION = {
+    "13.0": AndroidVersion.M,
+    "14.1": AndroidVersion.N,
+    "15.1": AndroidVersion.O,
+	"16.0": AndroidVersion.P,
+	"17.1": AndroidVersion.Q,
+	"18.1": AndroidVersion.R,
+	"19.0": AndroidVersion.S,
+}
