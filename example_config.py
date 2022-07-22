f4c7c01b0fbdb1000b49e773dd9c640fc57b0cd4
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2022-07-22 17:16:45 +0200

    homebot: Bye-bye bridgey
    Change-Id: Ib6d3552a12bd685ac40220fba47848ac97ccdf76

--- example_config.py
+++ example_config.py
@@ -10,65 +10,6 @@ config = {
 		"logging_chat_id": "",
 	},
 
-	# Module - Bridgey
-	"bridgey": {
-		# Enable bridging
-		# type: bool
-		"enabled": False,
-
-		# List of pools (don't change pools and platforms name after you start the bridge)
-		# type: dict[str, dict[str, dict]]
-		"pools": {
-			"example": {
-				"discord": {
-					# The name of the platform
-					# type: str
-					"platform": "Discord",
-
-					# ID of the channel to bridge
-					# type: int
-					"channel_id": 0,
-
-					# Bot token
-					# type: str
-					"token": "",
-				},
-
-				"matrix": {
-					# The name of the platform
-					# type: str
-					"platform": "Matrix",
-
-					# User ID of the bot account (e.g. @user:matrix.org)
-					# type: str
-					"username": "",
-
-					# Password of the bot account
-					# type: str
-					"password": "",
-
-					# URL of the Matrix server (e.g. https://matrix.org)
-					# type: str
-					"homeserver_url": "",
-
-					# The alias of the room you want to bridge (e.g. #room:matrix.org)
-					# type: str
-					"room_alias": "",
-				},
-
-				"telegram": {
-					# The name of the platform
-					# type: str
-					"platform": "Telegram",
-
-					# ID of the chat to bridge
-					# type: int
-					"chat_id": 0,
-				},
-			},
-		},
-	},
-
 	# Module - LineageOS updates
 	"lineageos_updates": {
 		# type: bool
